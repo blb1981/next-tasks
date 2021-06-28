0009b7b35e519b200cf805e1dd170e8310fa40dc
Brandon <brandonbland90@gmail.com>
2021-06-28 16:13:28 -0400

    Change fonts, add styles to checkboxes, buttons.

--- components/Filters.js
+++ components/Filters.js
@@ -1,8 +1,17 @@
+import styles from '../styles/Home.module.scss'
+
 const Filters = ({ filters, toggleShowCompleted }) => {
 	return (
 		<div>
 			<label htmlFor='showCompleted'>
-				Show completed? <input id='showCompleted' type='checkbox' checked={filters.showCompleted} onChange={toggleShowCompleted} />
+				Show completed?{' '}
+				<input
+					className={styles.checkbox}
+					id='showCompleted'
+					type='checkbox'
+					checked={filters.showCompleted}
+					onChange={toggleShowCompleted}
+				/>
 			</label>
 		</div>
 	)
--- components/Task.js
+++ components/Task.js
@@ -1,10 +1,14 @@
+import styles from '../styles/Home.module.scss'
+
 const Task = ({ task, toggleTask, deleteTask }) => {
 	return (
 		<>
 			<li>
-				<span style={{ textDecoration: task.isComplete ? 'line-through' : 'none' }}>{task.summary}</span> -{' '}
-				<input type='checkbox' defaultChecked={task.isComplete} onChange={() => toggleTask(task.id)} />{' '}
-				<button onClick={() => deleteTask(task.id)}>Delete</button>
+				<span style={{ textDecoration: task.isComplete ? 'line-through' : 'none' }}>{task.summary}</span>
+				<input className={styles.checkboxTask} type='checkbox' defaultChecked={task.isComplete} onChange={() => toggleTask(task.id)} />{' '}
+				<button className={styles.btnTask} onClick={() => deleteTask(task.id)}>
+					Delete
+				</button>
 			</li>
 		</>
 	)
